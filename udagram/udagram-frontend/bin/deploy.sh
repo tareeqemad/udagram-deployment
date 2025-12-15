737263ed9c39af4e47e464a8e6b80c7f7de87f7d
Tareq Elbawab <tareqelbawab94@gmail.com>
2025-12-15 12:08:55 +0200

    Fix frontend lint + make deploy.sh use AWS_BUCKET + enforce LF

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -5,9 +5,7 @@ set -euo pipefail
 
 echo "Deploying frontend to s3://${AWS_BUCKET}"
 
-
 aws s3 sync ./www "s3://${AWS_BUCKET}" --delete
-
 
 aws s3 cp ./www/index.html "s3://${AWS_BUCKET}/index.html" \
   --cache-control="max-age=0, no-cache, no-store, must-revalidate" \
--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,15 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-ft93gaw9.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udagram-api-dev.eba-ft93gaw9.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
